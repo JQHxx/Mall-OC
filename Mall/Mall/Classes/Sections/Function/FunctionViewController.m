16d591a2a3874cdaf8871e588a96f2370bb886c2
HJQ <v-huangjiaqian@midland.local>
2019-08-29 11:44:58 +0800

    自动布局框架使用

--- Mall/Mall/Classes/Sections/Function/FunctionViewController.m
+++ Mall/Mall/Classes/Sections/Function/FunctionViewController.m
@@ -17,7 +17,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.edgesForExtendedLayout = UIRectEdgeNone;
     self.navigationItem.title = @"首页";
+    [self setupUI];
     
     FunctionRequest *request = [[FunctionRequest alloc]init];
     request.cacheConfig = [[ICacheConfig alloc]init];
@@ -32,6 +34,67 @@
         NSLog(@"请求失败%@", error);
     }];
     
+}
+
+
+- (void) setupUI {
+    
+    UIView *redView = [[UIView alloc]init];
+    redView.backgroundColor = [UIColor redColor];
+    [self.view addSubview:redView];
+    
+    /*
+    [redView autoPinEdgeToSuperviewEdge:ALEdgeTop withInset:20];
+    [redView autoPinEdgeToSuperviewEdge:ALEdgeBottom];
+    [redView autoPinEdgeToSuperviewEdge:ALEdgeLeft withInset:20];
+    [redView autoPinEdgeToSuperviewEdge:ALEdgeRight];
+     */
+    
+    /*
+    [redView autoPinEdgesToSuperviewEdgesWithInsets:UIEdgeInsetsMake(15, 15, 15, 15)];
+     */
+    
+    /*
+    [redView autoCenterInSuperview];
+    [redView autoSetDimensionsToSize:CGSizeMake(100, 100)];
+     */
+    
+    /*
+    [redView autoPinEdgeToSuperviewEdge:ALEdgeLeft withInset:20];
+    [redView autoAlignAxisToSuperviewAxis:ALAxisHorizontal];
+    [redView autoSetDimensionsToSize:CGSizeMake(100, 100)];
+    */
+    
+    /*
+    [redView autoPinEdgeToSuperviewEdge:ALEdgeTop withInset:20];
+    [redView autoAlignAxisToSuperviewAxis:ALAxisVertical];
+    [redView autoSetDimensionsToSize:CGSizeMake(100, 100)];
+     */
+    
+    /*
+     //蓝色view的左边,距离红色view的右边为30
+     [self.blueView autoPinEdge:ALEdgeLeft toEdge:ALEdgeRight ofView:self.redView withOffset:30];
+     //蓝色view,红色view等宽等高
+     [@[self.blueView,self.redView] autoMatchViewsDimension:ALDimensionWidth];
+     [@[self.blueView,self.redView] autoMatchViewsDimension:ALDimensionHeight];
+     //蓝色view,红色view水平对齐
+     [self.blueView autoAlignAxis:ALAxisHorizontal toSameAxisOfView:self.redView];
+     
+     */
+    
+    /*
+     //蓝色View的高度是红色View高度的一半
+     [self.blueView autoMatchDimension:ALDimensionHeight toDimension:ALDimensionHeight ofView:self.redView withMultiplier:0.5];
+     */
+    
+    /*
+     NSArray *colorViews = @[self.redView, self.blueView, self.purpleView, self.greenView];
+     [colorViews autoSetViewsDimension:ALDimensionHeight toSize:40.f];
+     //间距为10,水平对齐,依次排列
+     [colorViews autoDistributeViewsAlongAxis:ALAxisHorizontal alignedTo:ALAttributeHorizontal withFixedSpacing:10.0 insetSpacing:YES matchedSizes:YES];
+     //红色view相对于其父view水平对齐
+     [self.redView autoAlignAxisToSuperviewAxis:ALAxisHorizontal];
+     */
 }
 
 

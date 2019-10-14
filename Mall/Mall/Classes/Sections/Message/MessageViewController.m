c088409d7324ae61c09738a7a662f2d8b375a9b1
HJQ <v-huangjiaqian@midland.local>
2019-10-14 10:38:02 +0800

    下拉刷新增加震动反馈

--- Mall/Mall/Classes/Sections/Message/MessageViewController.m
+++ Mall/Mall/Classes/Sections/Message/MessageViewController.m
@@ -22,11 +22,38 @@
     
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
     // [TopBannerNotificationsUtils alertWithMessage:@"退出登录"];
-    
+    [self feedbackGenerator];
+    /*
    NSString *result =  [@"1000000" convertAmount];
     NSLog(@"%@", result);
-    
-    
+     */
+
+}
+
+/**
+ @brief MJRefresh增加震动反馈
+ // 增加KVO监听
+ [_tableView.mj_header addObserver:self forKeyPath:@"state" options:NSKeyValueObservingOptionNew context:nil];
+ [_tableView.mj_footer addObserver:self forKeyPath:@"state" options:NSKeyValueObservingOptionNew context:nil];
+ 
+ - (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSString *,id> *)change context:(void *)context {
+     
+     if ([object isEqual:self.tableView.mj_header] && self.tableView.mj_header.state == MJRefreshStatePulling) {
+         [self feedbackGenerator];
+     }
+     else if ([object isEqual:self.tableView.mj_footer] && self.tableView.mj_footer.state == MJRefreshStatePulling) {
+         [self feedbackGenerator];
+     }
+ }
+ 
+ */
+
+- (void)feedbackGenerator {
+    if (@available(iOS 10.0, *)) {
+        UIImpactFeedbackGenerator *generator = [[UIImpactFeedbackGenerator alloc] initWithStyle:UIImpactFeedbackStyleMedium];
+        [generator prepare];
+        [generator impactOccurred];
+    }
 }
 
 
